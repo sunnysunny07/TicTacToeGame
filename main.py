82795314deed01f337a6702feef903366080d122
Sonia <58107179+sunnysunny07@users.noreply.github.com>
2021-01-11 23:36:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ board_keys = []
 for key in theBoard:
     board_keys.append(key)
 
-''' We will have to print the updated board after every move in the game and 
-    thus we will make a function in which we'll define the printBoard function
-    so that we can easily print the board everytime by calling this function. '''
+
 
 def printBoard(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
